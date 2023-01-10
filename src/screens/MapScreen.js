b30832014442849e638436577b60631c9f22165b
Jaskaran Singh <jaskaransingh4704@gmail.com>
2023-01-10 22:11:43 +0530

    complete price and distance calculation functionality

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,15 +1,29 @@
-import { View } from 'react-native';
+import { TouchableOpacity, View, Text } from 'react-native';
 
 import Map from './Map';
 import { MapScreenNavigators } from './../navigators/MapScreenNavigators';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
 
 const MapScreen = () => {
+    const navigation = useNavigation();
     return (
         <>
+            <TouchableOpacity
+                className={`p-2 bg-white rounded-full shadow-lg absolute top-16 left-5 z-50`}
+                onPress={() => navigation.navigate('HomeScreen')}
+            >
+                <Ionicons
+                    name="menu"
+                    size={35}
+                    color="gray"
+                    className={'mt-2'}
+                />
+            </TouchableOpacity>
             <View className={'h-1/2'}>
                 <Map />
             </View>
-            <MapScreenNavigators/>
+            <MapScreenNavigators />
         </>
     );
 };
