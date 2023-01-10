c3bb86ca0ba5e15dded0ab95b49a4cf77f185db3
Jaskaran Singh <jaskaransingh4704@gmail.com>
2023-01-10 22:21:30 +0530

    Fixed minnor issus

--- src/navigators/MapScreenNavigators.js
+++ src/navigators/MapScreenNavigators.js
@@ -1,14 +1,20 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigateCards from '../screens/NavigateCards';
 import RideOptionsCard from '../screens/RideOptionsCard';
+import { useSelector } from 'react-redux';
+import { selectDestination, selectOrigin } from './../../redux/slices/navSlice';
+
 
 export function MapScreenNavigators() {
+    const origin = useSelector(selectOrigin);
+    const destination = useSelector(selectDestination); 
     const Stack = createNativeStackNavigator();
     return (
         <Stack.Navigator
             screenOptions={{
-                headerShown: false
+                headerShown: false,
             }}
+            initialRouteName={ (destination && origin) ? 'RideOptionsCard' : 'NavigateCards'}
         >
             <Stack.Screen name={'NavigateCards'} component={NavigateCards} />
             <Stack.Screen
